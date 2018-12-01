92121ae7d54d9b5e37327b5e9dafc600fb1ab14c
Adam Pridmore <adam_pridmore@hotmail.com>
2018-12-01 21:16:30 +0000

    - Solved day 1 part 1

--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -1,19 +1,40 @@
 import java.util
 
+import scala.io.Source
+
 class Day1Test extends org.scalatest.FunSuite {
+  private def frequenciesTotal(inputText: String) = {
+    val inputLines =
+      inputText
+        .split("\\r\\n")
+        .map(Integer.parseInt)
+
+    inputLines.sum
+  }
+
   test("Day1_frequencies_total_example") {
 
-    val inputText = """+1
+    val inputText =
+      """+1
 -2
 +3
 +1"""
 
-    val inputLines =
-      inputText
-      .split("\\r\\n")
-      .map(Integer.parseInt)
-
-    val sum = inputLines.sum
+    val sum: Int = frequenciesTotal(inputText)
     println(sum)
+  }
+
+
+  test("Day_1_part1") {
+    var lines = Source.fromResource("readme.txt").getLines()
+    println(lines.mkString(","))
+
+    val data = Source.fromResource("day1/frequencies.txt").mkString
+
+    val sum = frequenciesTotal(data)
+
+    println(sum)
+
+    assert(sum === 553)
   }
 }
