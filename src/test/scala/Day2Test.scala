44164496341c00b558999853c0bf21a6c1006768
Adam Pridmore <adam_pridmore@hotmail.com>
2018-12-02 20:05:58 +0000

    solved day 2 part 1.

--- src/test/scala/Day2Test.scala
+++ src/test/scala/Day2Test.scala
@@ -1,3 +1,5 @@
+import scala.io.Source
+
 class Day2Test extends org.scalatest.FunSuite {
   def hasThreeDuplicates(str: String) : Boolean ={
     hasExactlyXDuplicates(str, 3)
@@ -26,5 +28,47 @@ class Day2Test extends org.scalatest.FunSuite {
 
   test("Three duplicates") {
     assert(hasThreeDuplicates("babbc") === true)
+  }
+
+  val exampleData = """abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab"""
+
+  test("parse_text_to_string_list"){
+    var list = parseTextToLines(exampleData)
+
+    assert(list.length === 7)
+    assert(list(0) === "abcdef")
+  }
+
+  test("solve_example_day"){
+    assert(day_2_solver(exampleData) === 12)
+  }
+
+  test("solve_day_part_1"){
+    val data = Source.fromResource("day2/input.txt").mkString
+
+    val result  = day_2_solver(data)
+
+    println(result)
+    assert(result === 9139)
+  }
+
+  private def day_2_solver(data: String): Int = {
+    val lines = parseTextToLines(data)
+
+    val duplicatesCount = lines.count(hasTwoDuplicates)
+    val triplicateCount = lines.count(hasThreeDuplicates)
+
+    val result = duplicatesCount * triplicateCount
+    result
+  }
+
+  private def parseTextToLines(data : String): Array[String] = {
+      data.split("\\r?\\n")
   }
 }
