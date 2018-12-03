cf71410eaaa142c81cfee23a23f9723a9b3b09c1
Adam Pridmore <adam_pridmore@hotmail.com>
2018-12-03 21:21:56 +0000

    Day 3 - part 1. Solved

--- src/test/scala/Day3Test.scala
+++ src/test/scala/Day3Test.scala
@@ -1,8 +1,12 @@
+import javax.xml.transform.Source
+
+import scala.io
+import scala.io.Source.fromResource
 import scala.util.matching.Regex
 
-case class Point2d(x : Int, y  : Int){}
+case class Point2d(x: Int, y: Int) {}
 
-case class Line(id : String , pos :Point2d, size : Point2d){}
+case class Line(id: String, pos: Point2d, size: Point2d) {}
 
 class Day3Test extends org.scalatest.FunSuite {
 
@@ -20,13 +24,34 @@ class Day3Test extends org.scalatest.FunSuite {
     Line(id, Point2d(xpos, ypos), Point2d(xsize, ysize))
   }
 
-  test("Parse Line"){
-    val lineText  = "#10 @ 20,30: 40x50"
+  test("Parse Line") {
+    val lineText = "#10 @ 20,30: 40x50"
 
     val line = parseLine(lineText)
     val expectedLine = Line("10", Point2d(20, 30), Point2d(40, 50))
 
     assert(line === expectedLine)
+  }
+
+  def applyLineToCloth(cloth: Array[Array[Int]], line: Line): Unit ={
+    for(x <- line.pos.x until line.pos.x + line.size.x ;
+        y <- line.pos.y until line.pos.y + line.size.y ){
+
+      cloth(x)(y) = cloth(x)(y) + 1
+    }
+  }
+
+  test("Count cloth with two or more claims") {
+    var cloth = Array.ofDim[Int](1001, 1001)
+
+    fromResource("day3/claims.txt")
+      .getLines()
+      .map(parseLine)
+      .foreach(line => {
+        applyLineToCloth(cloth, line)
+      })
+
+    println(cloth.flatten.count(cell => cell > 1))
   }
 
   //  #1 @ 1,3: 4x4
