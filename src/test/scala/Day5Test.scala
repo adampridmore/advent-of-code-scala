31b285c82bec843d1e922bf54f0650d6f579c95b
Adam Pridmore <adam_pridmore@hotmail.com>
2018-12-15 22:07:47 +0000

    Day 5 using recursion

--- src/test/scala/Day5Test.scala
+++ src/test/scala/Day5Test.scala
@@ -1,5 +1,6 @@
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Ignore}
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.io.Source.fromResource
 
@@ -20,8 +21,7 @@ class Day5Test extends FunSuite {
 
     var newData: mutable.ListBuffer[Char] = mutable.ListBuffer[Char]()
     var characters: mutable.ListBuffer[Char] = mutable.ListBuffer[Char]()
-    data.foreach(c=>characters.append(c))
-
+    data.foreach(c => characters.append(c))
     do {
       wasReaction = false
 
@@ -65,11 +65,96 @@ class Day5Test extends FunSuite {
 
   }
 
-  test("Real data solver"){
+  test("Real data solver") {
     val realData: String = fromResource("day5/data.txt").mkString
 
     val result = solver(realData)
 
     println(s"Length: ${result.length} : $result")
+
+    assert(result.length === 10450)
+  }
+
+  test("Simple solver") {
+    //    val data = "abc"
+    //    def solver2 (data: String) = {
+    //      data
+    //    }
+    //
+    //    solver2(data)
+
+    val list = List(1, 2, 3, 4, 5)
+
+    def myMap(list: List[Int]): List[Int] = {
+      list match {
+        case a :: Nil => List(a * 10)
+        case a :: tail => List(a * 10) ::: myMap(tail)
+      }
+    }
+
+    println(myMap(list).mkString(","))
+
+    def listToString(list: List[Int]): String = {
+      list match {
+        case a :: Nil => a.toString
+        case a :: tail => a.toString + "," + listToString(tail)
+      }
+    }
+
+    println(listToString(list))
+  }
+
+  //@tailrec
+  final def applyReaction2(list: List[Char]): List[Char] = {
+    list match {
+      case head :: Nil => List(head)
+      case a :: b :: tail if react(a, b) => applyReaction2(tail)
+      case head :: tail => List(head) ::: applyReaction2(tail)
+    }
+  }
+
+  test("Reaction2 abcd") {
+    var reacted = applyReaction2("abcd".toCharArray.toList).mkString("")
+    assert(reacted === "abcd")
+  }
+
+  test("Reaction2 abBc") {
+    var reacted = applyReaction2("abBc".toCharArray.toList).mkString("")
+    assert(reacted === "ac")
+  }
+
+  def solver2(data: String): String = {
+    var dataArray = data.toList
+    var done = false
+
+    do {
+      val reacted = applyReaction2(dataArray)
+      if (reacted == dataArray) {
+        done = true
+      } else {
+        dataArray = reacted
+      }
+    } while (!done)
+
+    dataArray.mkString("")
+  }
+
+  test("Reaction2 exampleData") {
+
+    var reactedData = solver2(exampleData)
+
+    println(exampleData + "\r\n" + reactedData)
+
+    assert(reactedData.mkString("") === "dabCBAcaDA")
+  }
+
+  ignore("Reaction2 Real data solver") {
+    val realData: String = fromResource("day5/data.txt").mkString
+
+    val result = solver2(realData)
+
+    println(s"Length: ${result.length} : $result")
+
+    assert(result.length === 10450)
   }
 }
