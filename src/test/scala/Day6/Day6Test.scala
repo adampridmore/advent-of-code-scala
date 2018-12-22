a216f351788e5d84689ff36f35c3c617426ce06f
Adam Pridmore <adam_pridmore@hotmail.com>
2018-12-22 22:32:07 +0000

    - Created grid without mutation

--- src/test/scala/Day6/Day6Test.scala
+++ src/test/scala/Day6/Day6Test.scala
@@ -2,6 +2,8 @@ package Day6
 
 import org.scalatest.FunSuite
 
+import scala.collection.immutable
+
 class Day6Test extends FunSuite {
   val exampleData =
     """
@@ -64,16 +66,33 @@ class Day6Test extends FunSuite {
   }
 
   test("Draw example data") {
-    val grid: Array[Array[Cell]] = Array.fill[Cell](10, 10)(EmptyCell())
-
     def intToLetter(i: Int): String = {
       val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
       letters(i).toString
+      //i.toString
     }
 
-    parseLines(exampleData)
-      .zipWithIndex
-      .foreach { case (line, i) => grid(line.x)(line.y) = Danger(intToLetter(i)) }
+    val dangerByPoint =
+      parseLines(exampleData)
+        .zipWithIndex
+        .map { case (line, i) => (Point(line.x, line.y), Danger(intToLetter(i))) }
+        .toMap
+
+    def asCell(c: Cell): Cell = c
+
+    def coordinateToCell(x: Int, y: Int): Cell = {
+      dangerByPoint.get(Point(x, y)) match {
+        case Some(danger: Danger) => asCell(danger)
+        case None => asCell(EmptyCell())
+      }
+    }
+
+    val grid: Array[Array[Cell]] =
+      (for (x <- 0 to 9) yield {
+        (for (y <- 0 to 9) yield {
+          coordinateToCell(x, y)
+        }).toArray
+      }).toArray
 
     printGrid(grid)
   }
