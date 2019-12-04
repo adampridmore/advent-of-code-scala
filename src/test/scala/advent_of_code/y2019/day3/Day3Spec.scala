6e39241ffb2bf5e2b5746f3a37795d42eb3e53a1
Adam Pridmore <adam_pridmore@hotmail.com>
2019-12-04 20:18:22 +0000

    - Day 3 - Tidy up

--- src/test/scala/advent_of_code/y2019/day3/Day3Spec.scala
+++ src/test/scala/advent_of_code/y2019/day3/Day3Spec.scala
@@ -2,50 +2,7 @@ package advent_of_code.y2019.day3
 
 import org.scalatest.{Matchers, WordSpec}
 
-import scala.collection.mutable
 import scala.io.Source.fromResource
-
-/*
---- Day 3: Crossed Wires ---
-The gravity assist was successful, and you're well on your way to the Venus refuelling station. During the rush back on Earth, the fuel management system wasn't completely installed, so that's next on the priority list.
-
-Opening the front panel reveals a jumble of wires. Specifically, two wires are connected to a central port and extend outward on a grid. You trace the path each wire takes as it leaves the central port, one wire per line of text (your puzzle input).
-
-The wires twist and turn, but the two wires occasionally cross paths. To fix the circuit, you need to find the intersection point closest to the central port. Because the wires are on a grid, use the Manhattan distance for this measurement. While the wires do technically cross right at the central port where they both start, this point does not count, nor does a wire count as crossing with itself.
-
-For example, if the first wire's path is R8,U5,L5,D3, then starting from the central port (o), it goes right 8, up 5, left 5, and finally down 3:
-
-...........
-...........
-...........
-....+----+.
-....|....|.
-....|....|.
-....|....|.
-.........|.
-.o-------+.
-...........
-Then, if the second wire's path is U7,R6,D4,L4, it goes up 7, right 6, down 4, and left 4:
-
-...........
-.+-----+...
-.|.....|...
-.|..+--X-+.
-.|..|..|.|.
-.|.-X--+.|.
-.|..|....|.
-.|.......|.
-.o-------+.
-...........
-These wires cross at two locations (marked X), but the lower-left one is closer to the central port: its distance is 3 + 3 = 6.
-
-Here are a few more examples:
-
-R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83 = distance 159
-R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
-*/
 
 class Day3Spec extends WordSpec with Matchers {
   def readInput(): (Seq[String], Seq[String]) = {
@@ -53,7 +10,7 @@ class Day3Spec extends WordSpec with Matchers {
       .getLines().toList
       .map(row => row.split(","))
 
-    (items(0), items(1))
+    (items.head, items(1))
   }
 
   case class Position(column: Int, row: Int) {
@@ -71,10 +28,10 @@ class Day3Spec extends WordSpec with Matchers {
       this
     }
 
-    def getCrosses(): Seq[Position] = {
+    def getCrosses: Seq[Position] = {
       for {
-        row <- 0 until grid.length
-        col <- 0 until grid(0).length
+        row <- grid.indices
+        col <- grid(0).indices
         if grid(row)(col) == crossCell
       } yield Position(col - size / 2, size / 2 - row)
     }
@@ -97,33 +54,33 @@ class Day3Spec extends WordSpec with Matchers {
       })
     }
 
-    def drawLineDown(start: Position)(length: Int)(lineCharacter: Char) = {
+    def drawLineDown(start: Position)(length: Int)(lineCharacter: Char): Unit = {
       drawPoints(for {
         i <- start.row + 1 to (start.row + length)
 
       } yield Position(start.column, i))(lineCharacter)
     }
 
-    def drawLineUp(start: Position)(length: Int)(lineCharacter: Char) = {
+    def drawLineUp(start: Position)(length: Int)(lineCharacter: Char): Unit = {
       drawPoints(for {
         i <- (start.row - length) until start.row
 
       } yield Position(start.column, i))(lineCharacter)
     }
 
-    def drawLineRight(start: Position)(length: Int)(lineCharacter: Char) = {
+    def drawLineRight(start: Position)(length: Int)(lineCharacter: Char): Unit = {
       drawPoints(for {
         i <- start.column + 1 to (start.column + length)
       } yield Position(i, start.row))(lineCharacter)
     }
 
-    def drawLineLeft(start: Position)(length: Int)(lineCharacter: Char) = {
+    def drawLineLeft(start: Position)(length: Int)(lineCharacter: Char): Unit = {
       drawPoints(for {
         i <- (start.column - length) until start.column
       } yield Position(i, start.row))(lineCharacter)
     }
 
-    def executeLine(line: Seq[String], lineCharacter: Char) = {
+    def executeLine(line: Seq[String], lineCharacter: Char): Unit = {
 
       var currentPosition = Position(size / 2, size / 2)
 
@@ -139,7 +96,7 @@ class Day3Spec extends WordSpec with Matchers {
         })
     }
 
-    private def emptyGrid() = {
+    private def emptyGrid(): Array[Array[Cell]] = {
 
       val grid = Array.fill(size, size)(emptyCell)
 
@@ -148,7 +105,7 @@ class Day3Spec extends WordSpec with Matchers {
       grid
     }
 
-    def print = {
+    def print(): Unit = {
       println(grid
         .map(row => row.mkString(""))
         .mkString("\n"))
@@ -157,12 +114,11 @@ class Day3Spec extends WordSpec with Matchers {
     val emptyCell: Cell = '.'
     val crossCell: Cell = 'x'
 
-    val grid = emptyGrid()
+    val grid: Array[Array[Cell]] = emptyGrid()
   }
 
   "Part I" should {
     "Example 1" in {
-      //      val (line1, line2) = readInput()
       val line1: Array[String] = "R8,U5,L5,D3".split(",")
       val line2: Array[String] = "U7,R6,D4,L4".split(",")
 
@@ -170,9 +126,9 @@ class Day3Spec extends WordSpec with Matchers {
 
       grid.solve(List(line1, line2))
 
-      grid.print
+      grid.print()
 
-      val minDistance = grid.getCrosses()
+      val minDistance = grid.getCrosses
         .map(x => x.manhattanDistance)
         .min
 
@@ -182,22 +138,13 @@ class Day3Spec extends WordSpec with Matchers {
     }
 
     "Example 1 - don't count crosses" in {
-      //      val (line1, line2) = readInput()
       val line1: Array[String] = "R8,U5,L5,D6".split(",")
 
       val grid = Grid(20)
 
       grid.solve(List(line1))
 
-      grid.print
-
-      //      val minDistance = grid.getCrosses()
-      //        .map(x => x.manhattanDistance)
-      //        .min
-      //
-      //      println(s"minDistance: $minDistance")
-      //
-      //      minDistance should be (159)
+      grid.print()
     }
 
     "Example 2" in {
@@ -209,10 +156,8 @@ class Day3Spec extends WordSpec with Matchers {
 
       grid.solve(List(line1, line2))
 
-      //      grid.print
-
       val crosses = grid
-        .getCrosses()
+        .getCrosses
 
       println(crosses.mkString(","))
 
@@ -220,10 +165,8 @@ class Day3Spec extends WordSpec with Matchers {
 
       println(s"minDistance: ${distances.min}")
 
-      // 159
+      distances.min should be (159)
     }
-
-
   }
 
   "part II" should {
@@ -232,11 +175,13 @@ class Day3Spec extends WordSpec with Matchers {
 
       val sol = Grid(18000)
         .solve(List(line1, line2))
-        .getCrosses()
+        .getCrosses
         .map(x => x.manhattanDistance)
         .min
 
       println(sol)
+
+      sol should be(731)
     }
   }
 }
